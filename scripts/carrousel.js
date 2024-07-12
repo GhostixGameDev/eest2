ea4b2f1e60333877c5f08536eadf6073e0eb663a
Haziel <business@ghostix.network>
2024-07-11 21:02:23 -0300

    Implementated move animation.

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -48,8 +48,10 @@ class Carrousel {
     //Instance methods
     restartIfFinalIndex = () => {
         //Si el slider llega a la ultima imagen se reinicia.
-        if (this.getSlideIndex() > this.getLength()) {
-            this.setSlideIndex(1);
+        if (this.getSlideIndex() >= this.getLength()) {
+            this.setSlideIndex(0);
+        }else if (this.getSlideIndex() < 0) {
+            this.setSlideIndex(this.getLength() - 1);
         }
     }
     animate = (animation) => {
@@ -61,38 +63,40 @@ class Carrousel {
                     this.getSlide(i).style.display = "none";
                 }
                 //Le sumamos 1 al indice de las imagenes.
-                this.setSlideIndex(this.getSlideIndex() + 1);
                 this.restartIfFinalIndex();
+                this.setSlideIndex(this.getSlideIndex() + 1);
                 this.getSlide(this.getSlideIndex()-1).style.display = "block";
                 break;
             //Animación de mover a la izquierda.
             case "move":
-
+                for (let i = 0; i < this.getLength(); i++) {
+                    this.getSlide(i).style.transition = "transform 0.5s ease-in-out";
+                    this.getSlide(i).style.transform = `translateX(${i - (this.getSlideIndex()) * 100}%)`;
+                }
+                this.setSlideIndex(this.getSlideIndex() + 1);
                 break;
             default:
                 break;
         }
     }
 
-    loop = (delay, fade) => {
-        this.animate(fade);
+    loop = (delay, animation) => {
+        this.animate(animation);
         if (this.getHasArrows()) {
             this.getArrow(0).onclick = () => {
+                this.setSlideIndex(this.getSlideIndex() - 2);
                 this.restartIfFinalIndex();
-                this.animate(fade);
+                this.animate(animation);
             };
 
             this.getArrow(1).onclick = () => {
-                this.setSlideIndex(this.getSlideIndex() - 2);
-                if (this.getSlideIndex() < 0) {
-                    this.setSlideIndex(this.getLength() - 1);
-                }
-                this.animate(fade);
+                this.animate(animation);
+                this.restartIfFinalIndex();
             };
         }
         if(delay != 0){
             setTimeout(() => {
-                this.loop(delay, fade);
+                this.loop(delay, animation);
             }, delay);
         }
     }
