ecb8654b61ab26ac3ee8de665af9a96cd229e52c
Haziel <hazielescuela1@gmail.com>
2024-11-14 11:38:27 -0300

    Fixed openTab function

--- public/scripts/TecnicaturasAnios.js
+++ public/scripts/TecnicaturasAnios.js
@@ -1,24 +1,17 @@
-const tabContents = document.querySelectorAll('.tab-content');
 function openTab(stylingTarget, tabId) {
-    // Ocultar todo el contenido de las pestañas
-    if(!tabContents) return;
+    const tabContents = document.querySelectorAll('.tab-content');
     const tab = document.getElementById(stylingTarget);
-    if(!tab) return;
-    closeTabs();
-    console.log(tab);
-    console.log(tabContents);
-    // Mostrar el contenido de la pestaña actual y añadir clase activa al botón
-    document.getElementById(tabId).style.display = 'block';
-    tab.classList.add('active');
-}
-
-function closeTabs(){
+    const tabButtons = document.querySelectorAll('.tab-button');
+    // Ocultar todo el contenido de las pestañas
+    if(!tabContents || !tab || !tabButtons) return;
     tabContents.forEach(content => {
         content.style.display = 'none';
     });
     // Quitar clase activa de todos los botones
-    const tabButtons = document.querySelectorAll('.tab-button');
     tabButtons.forEach(button => {
         button.classList.remove('active');
     });
+    // Mostrar el contenido de la pestaña actual y añadir clase activa al botón
+    document.getElementById(tabId).style.display = 'block';
+    tab.classList.add('active');
 }
