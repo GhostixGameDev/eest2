e6dc53c20d9fd7d08114e33532229651c4156610
Haziel <hazielescuela1@gmail.com>
2024-11-14 15:35:29 -0300

    started completing one section of index

--- public/scripts/Util.js
+++ public/scripts/Util.js
@@ -1,6 +1,16 @@
 //Funcion que extiende el menu de la sidebar y demas.
 function extendDiv(objective){
 	const object = document.querySelector(objective);
-	console.log(object);
 	object.classList.toggle("active");
+}
+// Funcion que extiende la sección de tecnicaturas en index.
+function openTecnicatura(event, objective, url){
+	const container = document.getElementById(objective);
+	if(!container || !event) return;
+	if(!container.classList.contains("active")){
+		extendDiv(container);
+		extendDiv(event.currentTarget.classList);
+		return;
+	}
+	
 }
