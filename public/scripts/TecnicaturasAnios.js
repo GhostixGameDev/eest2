5e95b45f23ce8b0612455d8de050dbfd5a2e8341
Kaito090 <kaitohiroshi090@gmail.com>
2024-11-14 10:59:04 -0300

    AwA

--- public/scripts/TecnicaturasAnios.js
+++ public/scripts/TecnicaturasAnios.js
@@ -1,17 +1,24 @@
-function openTab(event, tabId) {
+const tabContents = document.querySelectorAll('.tab-content');
+function openTab(stylingTarget, tabId) {
     // Ocultar todo el contenido de las pestañas
-    const tabContents = document.querySelectorAll('.tab-content');
+    if(!tabContents) return;
+    const tab = document.getElementById(stylingTarget);
+    if(!tab) return;
+    closeTabs();
+    console.log(tab);
+    console.log(tabContents);
+    // Mostrar el contenido de la pestaña actual y añadir clase activa al botón
+    document.getElementById(tabId).style.display = 'block';
+    tab.classList.add('active');
+}
+
+function closeTabs(){
     tabContents.forEach(content => {
         content.style.display = 'none';
     });
-
     // Quitar clase activa de todos los botones
     const tabButtons = document.querySelectorAll('.tab-button');
     tabButtons.forEach(button => {
         button.classList.remove('active');
     });
-
-    // Mostrar el contenido de la pestaña actual y añadir clase activa al botón
-    document.getElementById(tabId).style.display = 'block';
-    event.currentTarget.classList.add('active');
 }
